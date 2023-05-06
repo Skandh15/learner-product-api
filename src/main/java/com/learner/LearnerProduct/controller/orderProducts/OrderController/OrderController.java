997c5e2b23b5554ef740abe6075ee21a1b5586b4
skandh.15 <skandhoff@gmail.com>
2023-05-06 21:45:13 +0530

    Added code to call get-order-details using userId.

--- src/main/java/com/learner/LearnerProduct/controller/orderProducts/OrderController/OrderController.java
+++ src/main/java/com/learner/LearnerProduct/controller/orderProducts/OrderController/OrderController.java
@@ -1,12 +1,16 @@
 package com.learner.LearnerProduct.controller.orderProducts.OrderController;
 
 import com.learner.LearnerProduct.controller.orderProducts.Beans.OrderBean;
+import com.learner.LearnerProduct.controller.orderProducts.UserById;
+import com.learner.LearnerProduct.controller.orderProducts.UserClient;
 import com.learner.LearnerProduct.entity.Order;
 import com.learner.LearnerProduct.repository.OrderRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -15,6 +19,8 @@ import java.util.List;
 public class OrderController {
     @Autowired
     private OrderRepository orderRepository;
+    @Autowired
+    private RestTemplate restTemplate;
 
     // Create an order
     @PostMapping("/place-order")
@@ -36,9 +42,25 @@ public class OrderController {
         }
     }
 
-    // Get Order
+    // Get Order using userId
     @GetMapping("get-order-details")
-    public List<Order> getOrders(@RequestParam String emailId) {
+    public ResponseEntity<Object> getOrders(@RequestParam long userId, HttpServletResponse response) {
+        UserClient userClient = new UserClient();
+        UserById user = userClient.getUserDetails(restTemplate, userId);
+        if (user == null) {
+            return ResponseEntity.noContent().build();
+        } else {
+            try {
+                // code to get order details using the user object
+                List<Order> ordersList = getOrderDetails(user.getEmail());
+                return ResponseEntity.ok(ordersList);
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+            }
+        }
+    }
+
+    private List<Order> getOrderDetails(String emailId) {
         return this.orderRepository.findAllByEmailId(emailId);
     }
 }
